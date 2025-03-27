22c6b105b9a3b2fcd4ad18aa74f4ba1b83286690
Abhay <toheebabiodun03@gmail.com>
2025-03-27 13:57:38 +0100

    feat: user can create posts and view all posts created

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-@app.get("/users", response_model=list[User], tags=['User'])
+@app.get("/users", response_model=list[User], tags=['users'])
 async def list_users(session: Session = Depends(get_session)):
     users = session.exec(select(User)).all()
     return users
 
-@app.post("/users", response_model=User, tags=['User'])
+@app.post("/users", response_model=User, tags=['users'])
 async def create_user(user: User, session: Session = Depends(get_session)):
     user = User(name=user.name, email=user.email)
     session.add(user)
@@ -24,14 +24,14 @@ async def create_user(user: User, session: Session = Depends(get_session)):
     session.refresh(user)
     return user
 
-@app.get("/users/{user_id}", response_model=User, tags=["User"])
+@app.get("/users/{user_id}", response_model=User, tags=["users"])
 async def get_user(user_id: int, session: Session = Depends(get_session)):
     user = session.get(User, user_id)
     if not user:
         raise HTTPException(status_code=404, detail="User is not found")
     return user
 
-@app.put("/users/{user_id}", response_model=User, tags=["User"])
+@app.put("/users/{user_id}", response_model=User, tags=["users"])
 async def update_user(user_id: int, user: User, session: Session = Depends(get_session)):
     get_user = session.get(User, user_id)
     if not get_user:
@@ -44,7 +44,7 @@ async def update_user(user_id: int, user: User, session: Session = Depends(get_s
     session.refresh(get_user) 
     return user
 
-@app.delete("/users/{user_id}", status_code=204, tags=["User"])
+@app.delete("/users/{user_id}", status_code=204, tags=["users"])
 async def delete_user(user_id: int, session: Session = Depends(get_session)):
     user = session.get(User, user_id)
     if not user:
@@ -52,3 +52,39 @@ async def delete_user(user_id: int, session: Session = Depends(get_session)):
     session.delete(user)
     session.commit()
     return {"detail" : "User is deleted"}
+
+#POST
+
+@app.get("/posts", response_model=list[Post], tags=["posts"])
+async def list_posts(session: Session = Depends(get_session)):
+    posts = session.exec(select(Post)).all()
+    return posts
+
+@app.get("/posts/{post_id}", response_model=Post, tags=["posts"])
+async def get_post(post_id: int, session: Session = Depends(get_session)):
+    post = session.get(Post, post_id)
+    if not post: 
+        raise HTTPException(status_code=404, detail="Post not found")
+    return post
+
+
+
+#User Posts
+@app.get("/users/{user_id}/posts", response_model=list[Post], tags=["user posts"])
+async def list_user_posts(user_id: int, session: Session = Depends(get_session)):
+    user = session.get(User, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user.posts
+
+@app.post("/users/{user_id}/posts", response_model=Post, tags=["user posts"])
+async def create_user_post(user_id: int, post: Post, session: Session = Depends(get_session)):
+    user = session.get(User, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    post = Post(title = post.title, content=post.content)
+    user.posts.append(post)
+    session.add(user)
+    session.commit()
+    session.refresh(post)
+    return post
